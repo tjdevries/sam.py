466865506791ad87f6c65c4a4599111ce24586be
TJ DeVries <devries.timothyj@gmail.com>
2022-03-15 16:32:27 -0400

    add local var

--- src/sam.py
+++ src/sam.py
@@ -1,13 +1,14 @@
 from .exporter import exported_func
 
-def my_func() -> None:
+def my_func() -> int:
     """This is my function"""
-    pass
+    return 5
 
 def other_func() -> None:
     """Wow, this calls some stuff"""
-    my_func()
+    x = my_func()
     exported_func()
+    print(x)
 
 if __name__ == "__main__":
     my_func()
