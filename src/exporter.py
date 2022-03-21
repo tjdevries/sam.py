c77ec76f9ef1e03da28043fec9e6250ec0a13af0
TJ DeVries <devries.timothyj@gmail.com>
2022-03-21 15:29:14 -0400

    bump

--- src/exporter.py
+++ src/exporter.py
@@ -6,5 +6,8 @@ def my_decorator(f):
 def exported_func(a: str, b: str = ", World!") -> str:
     """This is the
     docstring from
-    an exported function"""
+    an exported function
+
+    :param a: Describing cool parameter `a`
+    """
     return a + b
