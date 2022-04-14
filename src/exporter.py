87712958455b90fe8d0fb1a0fc110b41d6c3a361
TJ DeVries <devries.timothyj@gmail.com>
2022-04-14 14:11:19 -0400

    add docstrings

--- src/exporter.py
+++ src/exporter.py
@@ -1,3 +1,7 @@
+"""
+This file is just for exporting stuffs :)
+"""
+
 def my_decorator(f):
     return f
 
--- src/sam.py
+++ src/sam.py
@@ -1,3 +1,7 @@
+"""
+This is the file that actually does things
+"""
+
 import json
 
 import requests
