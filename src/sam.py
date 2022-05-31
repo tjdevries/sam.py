998c185ce90adff3ed7c2c357d066a1f9d264de7
TJ DeVries <devries.timothyj@gmail.com>
2022-05-31 13:46:12 -0400

    small

--- src/sam.py
+++ src/sam.py
@@ -4,49 +4,9 @@ This is the file that actually does things
 CI Attempt: 2
 """
 
-import json
+class ShouldConsole:
+    def __init__(self):
+        self.x = 1
 
-import requests
-import leftpad
-
-import exporter
-from .exporter import exported_func
-from .exporter import ExportedClass
-
-default_val = 6
-default_val = 8
-
-EXPORTED_CLASS = ExportedClass()
-
-def my_func(val=default_val) -> int:
-    """This is my function"""
-    x = 10
-    x = 12
-    return x * (val or default_val)
-
-def func_should_return_int(x: int):
-    return x * 2
-
-def other_func() -> None:
-    """Wow, this calls some stuff"""
-    x = my_func()
-    exported_func(abc="hello")
-    print(ThisClass(x))
-
-def uses_json() -> None:
-    print(json.loads('{"hello": "world"}'))
-
-
-class ThisClass:
-    a: int
-    """Not going to lie, I hate that this comes after the property"""
-
-    def __init__(self, a: int):
-        self.a = a
-
-if __name__ == '__main__':
-    my_func()
-    requests.get("https://google.com")
-
-    print(exporter.exported_func == exported_func)
-
+    def other(self):
+        print(self.x)
