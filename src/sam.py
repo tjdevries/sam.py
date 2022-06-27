91d386da1a5f3e36bec823e107612b556adb7416
TJ DeVries <devries.timothyj@gmail.com>
2022-06-27 15:32:00 -0400

    add dep

--- src/sam.py
+++ src/sam.py
@@ -4,9 +4,12 @@ This is the file that actually does things
 CI Attempt: 2
 """
 
+import sqlparse
+
 class ShouldConsole:
     def __init__(self):
         self.x = 1
+        print(sqlparse)
 
     def other(self):
         print(self.x)
